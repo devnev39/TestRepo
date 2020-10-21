c95ee1a5fde57d3f8c591a986b519834a68db5ed
Dhruv <devnev39@gmail.com>
2020-10-21 09:16:30 +0530

    commiting both files

---
+++ Hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     cout<<"Hello World !"<<endl;
+     cout<<"In Hello.cpp file"<<endl;
+     return 0;
+}
---
+++ Local.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"hello world."<<std::endl;
+    std::cout<<"This is for git update"<<std::endl;
+    std::cout<<"This is for git update on dev branch"<<std::endl;
+    return 0;
+}
